fee46cb54c22ddbaa08841da867d9d02388829a6
Ishak MP <ishqmp@gmail.com>
2024-02-12 01:40:16 +0530

    Refactor: updated after a while

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require("http");
 dotenv.config({ path: "./config/config.env" });
 
 const app = require("./app");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.set("port", port);
 
 const server = http.createServer(app);
